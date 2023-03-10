d2cf6822e7c56fe2d85af415f093284d92a6c979
Hossein Fadaei <mehrdadfadaee4@gmail.com>
2023-03-10 21:41:38 +0330

    add-js-code-for-field

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,19 @@ toggler.addEventListener("click" ,(e) => {
     navbar.classList.toggle("nav__expanded");
 });
     
+
+const form = document.querySelector('#myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name');
+  const email = document.querySelector('#email');
+
+  if (name.value === '' || email.value === '') {
+    alert('لطفا فیلد های مورد نظر را تکمیل کنید');
+  } else {
+    // send form data to server
+    form.submit();
+  }
+});
